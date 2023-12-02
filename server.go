2d4d06a7b2486eac23fb12d9cbffec5dc14068a8
OptimusePrime <karlo.vizec@skole.hr>
2023-12-02 19:56:26 +0100

    changes

--- server.go
+++ server.go
@@ -13,14 +13,14 @@ import (
     "time"
 )
 
-type RunFunction func(m *module.Module, backendPort module.Port)
+type RunFunction func(m *module.Module)
 
 type HandshakeHandler struct {
     RegisteredCommands []*command.Command
     Module             *module.Module
     Run                RunFunction
-    BackendPort        module.Port
-    FrontendPort       module.Port
+    RPCPort            module.Port
+    MainPort           module.Port
 }
 
 type HandshakeResponse struct {
@@ -28,13 +28,12 @@ type HandshakeResponse struct {
 }
 
 func (h *HandshakeHandler) OnHandshake(config module.Configuration) HandshakeResponse {
-    log.Println("Hands have been shook")
     h.Module = &module.Module{
         Port:     config.Port,
         Manifest: config.Manifest,
     }
 
-    defer h.Run(h.Module, h.BackendPort)
+    defer h.Run(h.Module)
 
     return HandshakeResponse{
         RegisteredCommands: h.RegisteredCommands,
@@ -42,41 +41,35 @@ func (h *HandshakeHandler) OnHandshake(config module.Configuration) HandshakeRes
 }
 
 func InitModule(run RunFunction) error {
-    log.Println("Hands have been shook 1")
-    backendPort, _ := strconv.Atoi(os.Getenv("MODULE_BACKEND_PORT"))
-    frontendPort, _ := strconv.Atoi(os.Getenv("MODULE_FRONTEND_PORT"))
-    // rpcPort, _ := strconv.Atoi(os.Getenv("MODULE_RPC_PORT"))
+    rpcPort, _ := strconv.Atoi(os.Getenv("MODULE_RPC_PORT"))
+    mainPort, _ := strconv.Atoi(os.Getenv("MODULE_MAIN_PORT"))
 
-    log.Println("Hands have been shook 2")
     rpcServer := jsonrpc.NewServer()
 
     handshakeHandler := new(HandshakeHandler)
     handshakeHandler.Run = run
-    handshakeHandler.BackendPort = module.Port(backendPort)
-    handshakeHandler.FrontendPort = module.Port(frontendPort)
+    handshakeHandler.RPCPort = module.Port(rpcPort)
+    handshakeHandler.MainPort = module.Port(mainPort)
 
-    log.Println("Hands have been shook 3")
     rpcServer.Register("HandshakeHandler", handshakeHandler)
 
-    listener, err := wasip1.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", 8092))
+    listener, err := wasip1.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", rpcPort))
     if err != nil {
         log.Fatalf("Failed creating TCP listener: %e", err)
         return err
     }
-    log.Println("Hands have been shook 4")
 
     server := http.Server{
         Handler:      rpcServer,
         ReadTimeout:  5 * time.Second,
         WriteTimeout: 5 * time.Second,
-        Addr:         fmt.Sprintf(":%v", 8092),
+        Addr:         fmt.Sprintf(":%v", rpcPort),
     }
 
     if err = server.Serve(listener); err != nil {
         log.Fatalf("Failed starting RPC server: %e", err)
         return err
     }
-    log.Println("Hands have been shook 5")
 
     return nil
 }
