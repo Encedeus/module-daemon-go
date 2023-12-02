d221c7bb3a6751e6b53ea1a52b60118c77bf5086
OptimusePrime <karlo.vizec@skole.hr>
2023-12-02 19:03:09 +0100

    logging

--- server.go
+++ server.go
@@ -28,7 +28,7 @@ type HandshakeResponse struct {
 }
 
 func (h *HandshakeHandler) OnHandshake(config module.Configuration) HandshakeResponse {
-    go log.Println("Hands have been shook")
+    log.Println("Hands have been shook")
     h.Module = &module.Module{
         Port:     config.Port,
         Manifest: config.Manifest,
@@ -45,7 +45,7 @@ func InitModule(run RunFunction) error {
     log.Println("Hands have been shook 1")
     backendPort, _ := strconv.Atoi(os.Getenv("MODULE_BACKEND_PORT"))
     frontendPort, _ := strconv.Atoi(os.Getenv("MODULE_FRONTEND_PORT"))
-    rpcPort, _ := strconv.Atoi(os.Getenv("MODULE_RPC_PORT"))
+    // rpcPort, _ := strconv.Atoi(os.Getenv("MODULE_RPC_PORT"))
 
     log.Println("Hands have been shook 2")
     rpcServer := jsonrpc.NewServer()
@@ -58,9 +58,10 @@ func InitModule(run RunFunction) error {
     log.Println("Hands have been shook 3")
     rpcServer.Register("HandshakeHandler", handshakeHandler)
 
-    listener, err := wasip1.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", rpcPort))
+    listener, err := wasip1.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", 8092))
     if err != nil {
         log.Fatalf("Failed creating TCP listener: %e", err)
+        return err
     }
     log.Println("Hands have been shook 4")
 
@@ -68,14 +69,13 @@ func InitModule(run RunFunction) error {
         Handler:      rpcServer,
         ReadTimeout:  5 * time.Second,
         WriteTimeout: 5 * time.Second,
-        Addr:         fmt.Sprintf(":%v", rpcPort),
+        Addr:         fmt.Sprintf(":%v", 8092),
     }
 
-    go func() {
-        if err = server.Serve(listener); err != nil {
-            log.Fatalf("Failed starting RPC server: %e", err)
-        }
-    }()
+    if err = server.Serve(listener); err != nil {
+        log.Fatalf("Failed starting RPC server: %e", err)
+        return err
+    }
     log.Println("Hands have been shook 5")
 
     return nil
