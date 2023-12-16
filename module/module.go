9d1c1d7a7810b206a2d849785ab24d4f4a513da0
OptimusePrime <karlo.vizec@skole.hr>
2023-12-16 18:22:46 +0100

    debugging information

--- module/module.go
+++ module/module.go
@@ -4,7 +4,6 @@ import (
     "context"
     "fmt"
     "github.com/filecoin-project/go-jsonrpc"
-    "time"
 )
 
 type Result any
@@ -90,7 +89,7 @@ func (m *Module) Invoke(cmd string, args Arguments) (Result, error) {
         ModuleInvoke InvokeFunc
     }
     fmt.Printf("Host port: %v\n", m.HostPort)
-    time.Sleep(2 * time.Second)
+    // time.Sleep(2 * time.Second)
 
     closer, err := jsonrpc.NewClient(context.Background(), fmt.Sprintf("http://localhost:%v", m.HostPort), "ModuleInvokeHandler", &client, nil)
     if err != nil {
