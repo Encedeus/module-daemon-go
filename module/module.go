1e02bf7f058e94b2eb5746f4497720b57567e4d9
OptimusePrime <karlo.vizec@skole.hr>
2024-02-19 16:56:00 +0100

    spawn main module process in goroutine

--- module/module.go
+++ module/module.go
@@ -1,9 +1,9 @@
 package module
 
 import (
-    "context"
-    "fmt"
-    "github.com/filecoin-project/go-jsonrpc"
+	"context"
+	"fmt"
+	"github.com/filecoin-project/go-jsonrpc"
 )
 
 type Result any
@@ -12,112 +12,115 @@ type Arguments map[string]any
 type Executor func(m *Module, args Arguments) (Result, error)
 
 type Command struct {
-    Name   string
-    Params Parameters
-    Exec   Executor
+	Name   string
+	Params Parameters
+	Exec   Executor
 }
 
 type InvokeFunc func(command string, args Arguments) (Result, error)
 
 type HostInvokeHandler struct {
-    Module *Module
+	Module *Module
 }
 
 func (h *HostInvokeHandler) HostInvoke(command string, args Arguments) (Result, error) {
-    for _, cmd := range h.Module.Commands {
-        if cmd.Name == command {
-            result, err := cmd.Exec(h.Module, args)
-            if err != nil {
-                return nil, err
-            }
+	for _, cmd := range h.Module.Commands {
+		if cmd.Name == command {
+			result, err := cmd.Exec(h.Module, args)
+			if err != nil {
+				return nil, err
+			}
 
-            return result, nil
-        }
-    }
+			return result, nil
+		}
+	}
 
-    return nil, nil
+	return nil, nil
 }
 
 type RunFunction func(m *Module)
 
 type HandshakeHandler struct {
-    RegisteredCommands []*Command
-    Module             *Module
-    Run                RunFunction
-    RPCPort            Port
-    MainPort           Port
+	RegisteredCommands []*Command
+	Module             *Module
+	Run                RunFunction
+	RPCPort            Port
+	MainPort           Port
 }
 
 type HandshakeResponse struct {
-    // RegisteredCommands []*command.Command
+	// RegisteredCommands []*command.Command
 }
 
 func (h *HandshakeHandler) OnHandshake(config Configuration) HandshakeResponse {
-    h.Module.Port = config.Port
-    h.Module.Manifest = config.Manifest
-    h.Module.HostPort = config.HostPort
-    h.Module.HandshakeHandler = h
+	h.Module.Port = config.Port
+	h.Module.Manifest = config.Manifest
+	h.Module.HostPort = config.HostPort
+	h.Module.HandshakeHandler = h
 
-    defer h.Run(h.Module)
-    fmt.Println("Handshake done from module side")
+	defer func() {
+		go func() {
+			h.Run(h.Module)
+		}()
+	}()
 
-    return HandshakeResponse{}
+	return HandshakeResponse{}
 }
 
 type Manifest struct {
-    Name             string   `hcl:"name"`
-    Authors          []string `hcl:"authors"`
-    Version          string   `hcl:"version"`
-    FrontendMainFile string   `hcl:"frontend_main"`
-    // BackendMainFile  string   `hcl:"backend_main"`
+	Name             string   `hcl:"name"`
+	Authors          []string `hcl:"authors"`
+	Version          string   `hcl:"version"`
+	FrontendMainFile string   `hcl:"frontend_main"`
+	// BackendMainFile  string   `hcl:"backend_main"`
 }
 
 type Module struct {
-    Port             Port
-    Manifest         Manifest
-    HostPort         Port
-    Commands         []*Command
-    HandshakeHandler *HandshakeHandler
+	Port             Port
+	Manifest         Manifest
+	HostPort         Port
+	Commands         []*Command
+	HandshakeHandler *HandshakeHandler
 }
 
 func (m *Module) RegisterCommand(cmd Command) {
-    m.Commands = append(m.Commands, &cmd)
+	m.Commands = append(m.Commands, &cmd)
 }
 
 func (m *Module) Invoke(cmd string, args Arguments) (Result, error) {
-    var client struct {
-        ModuleInvoke InvokeFunc
-    }
-    fmt.Printf("Host port: %v\n", m.HostPort)
-    // time.Sleep(2 * time.Second)
+	var client struct {
+		ModuleInvoke InvokeFunc
+	}
+	fmt.Printf("Host port: %v\n", m.HostPort)
+	// time.Sleep(2 * time.Second)
 
-    /*    conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%v", m.HostPort))
-          fmt.Printf("Invoke dial error: %e\n", err)
-          if err != nil {
-              log.Fatalf("%e", err)
-          }
-          defer conn.Close()*/
+	/*    conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%v", m.HostPort))
+	      fmt.Printf("Invoke dial error: %e\n", err)
+	      if err != nil {
+	          log.Fatalf("%e", err)
+	      }
+	      defer conn.Close()*/
 
-    closer, err := jsonrpc.NewClient(context.Background(), fmt.Sprintf("http://localhost:%v", m.HostPort), "ModuleInvokeHandler", &client, nil)
-    fmt.Printf("Client open error: %e\n", err)
-    if err != nil {
-        return nil, err
-    }
-    defer closer()
+	closer, err := jsonrpc.NewClient(context.Background(), fmt.Sprintf("http://localhost:%v", m.HostPort), "ModuleInvokeHandler", &client, nil)
+	fmt.Printf("Client open error: %e\n", err)
+	if err != nil {
+		return nil, err
+	}
+	defer closer()
 
-    result, err := client.ModuleInvoke(cmd, args)
-    fmt.Printf("Module invoke error: %e\n", err)
-    if err != nil {
-        return nil, err
-    }
+	result, err := client.ModuleInvoke(cmd, args)
+	fmt.Printf("Module invoke error: %e\n", err)
+	if err != nil {
+		return nil, err
+	}
 
-    return result, nil
+	return result, nil
 }
 
 type Port uint16
 
 type Configuration struct {
-    Port     Port
-    HostPort Port
-    Manifest Manifest
+	Port     Port
+	HostPort Port
+	Manifest Manifest
 }
