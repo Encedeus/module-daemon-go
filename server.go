9dffaa76078aa9cbdb93cf8b677a4cbc4c445773
OptimusePrime <karlo.vizec@skole.hr>
2024-02-19 16:39:23 +0100

    RPC server starts in goroutine

--- server.go
+++ server.go
@@ -1,50 +1,52 @@
 package daemon
 
 import (
-    "fmt"
-    "github.com/Encedeus/module-daemon-go/module"
-    "github.com/filecoin-project/go-jsonrpc"
-    "github.com/stealthrocket/net/wasip1"
-    "log"
-    "net/http"
-    "os"
-    "strconv"
-    "time"
+	"fmt"
+	"github.com/Encedeus/module-daemon-go/module"
+	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/stealthrocket/net/wasip1"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 func InitModule(run module.RunFunction) {
-    rpcPort, _ := strconv.Atoi(os.Getenv("MODULE_RPC_PORT"))
-    mainPort, _ := strconv.Atoi(os.Getenv("MODULE_MAIN_PORT"))
-    fmt.Printf("Perceived RPC port: %v\n", rpcPort)
+	rpcPort, _ := strconv.Atoi(os.Getenv("MODULE_RPC_PORT"))
+	mainPort, _ := strconv.Atoi(os.Getenv("MODULE_MAIN_PORT"))
+	fmt.Printf("Perceived RPC port: %v\n", rpcPort)
 
-    rpcServer := jsonrpc.NewServer()
+	rpcServer := jsonrpc.NewServer()
 
-    mod := new(module.Module)
+	mod := new(module.Module)
 
-    handshakeHandler := new(module.HandshakeHandler)
-    handshakeHandler.RPCPort = module.Port(rpcPort)
-    handshakeHandler.MainPort = module.Port(mainPort)
-    handshakeHandler.Module = mod
-    handshakeHandler.Run = run
+	handshakeHandler := new(module.HandshakeHandler)
+	handshakeHandler.RPCPort = module.Port(rpcPort)
+	handshakeHandler.MainPort = module.Port(mainPort)
+	handshakeHandler.Module = mod
+	handshakeHandler.Run = run
 
-    hostInvokeHandler := new(module.HostInvokeHandler)
-    hostInvokeHandler.Module = mod
+	hostInvokeHandler := new(module.HostInvokeHandler)
+	hostInvokeHandler.Module = mod
 
-    rpcServer.Register("HandshakeHandler", handshakeHandler)
+	rpcServer.Register("HandshakeHandler", handshakeHandler)
 
-    listener, err := wasip1.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", rpcPort))
-    if err != nil {
-        log.Fatalf("Failed creating TCP listener: %e", err)
-    }
+	listener, err := wasip1.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", rpcPort))
+	if err != nil {
+		log.Fatalf("Failed creating TCP listener: %v", err)
+	}
 
-    server := http.Server{
-        Handler:      rpcServer,
-        ReadTimeout:  5 * time.Second,
-        WriteTimeout: 5 * time.Second,
-        Addr:         fmt.Sprintf(":%v", rpcPort),
-    }
+	server := http.Server{
+		Handler:      rpcServer,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		Addr:         fmt.Sprintf(":%v", rpcPort),
+	}
 
-    if err = server.Serve(listener); err != nil {
-        log.Fatalf("Failed starting RPC server: %e", err)
-    }
+	go func() {
+		if err = server.Serve(listener); err != nil {
+			log.Fatalf("Failed starting RPC server: %v", err)
+		}
+	}()
 }
