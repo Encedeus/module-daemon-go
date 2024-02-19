bef9e22eb92d9e79c8f312d1ca8b0f32d5531c60
OptimusePrime <karlo.vizec@skole.hr>
2024-02-19 17:43:52 +0100

    registered HostInvokeHandler

--- server.go
+++ server.go
@@ -31,6 +31,7 @@ func InitModule(run module.RunFunction) {
 	hostInvokeHandler.Module = mod
 
 	rpcServer.Register("HandshakeHandler", handshakeHandler)
+	rpcServer.Register("HostInvokeHandler", hostInvokeHandler)
 
 	listener, err := wasip1.Listen("tcp", fmt.Sprintf("127.0.0.1:%v", rpcPort))
 	if err != nil {
